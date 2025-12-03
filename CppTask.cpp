af98f6d4643b4dce85b0195ba6ab454b040ed190
TurkerM <turkermustafakilic@gmail.com>
2025-12-03 14:30:06 +0300

    Interface (abstract) and classes added

--- CppTask.cpp
+++ CppTask.cpp
@@ -1,5 +1,29 @@
-#include <iostream>
+﻿#include <iostream>
 
+class IBaseClass {
+public:
+    virtual void PublicFunc() {
+        std::cout << "Public in base" << std::endl;
+    }
+
+private:
+    virtual void PrivateFunc() {
+        std::cout << "Private in base" << std::endl;
+    }
+
+protected:
+    virtual void ProtectedFunc() {
+        std::cout << "Protected in base" << std::endl;
+    }
+};
+
+class Child1 : public IBaseClass {
+    int field1;
+};
+
+class Child2 : public IBaseClass {
+    int field2;
+};
 
 int main()
 {
