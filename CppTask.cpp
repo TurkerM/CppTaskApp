6037b91b8210f891727392f027529d80bb6383d2
TurkerM <turkermustafakilic@gmail.com>
2025-12-04 11:51:05 +0300

    Test için yazılan bazı print satırları kaldırıldı. Periyod sağlaması için olan 1 saniyelik delay fonksiyonu kullanıma alındı. Instance arama kodları döngüden çıkarıldı. Çıktılar Türkçe'ye çevrildi. Kodun anlaşılırlığı için yorum satırları eklendi.

--- CppTask.cpp
+++ CppTask.cpp
@@ -6,6 +6,7 @@
 #include <list>
 #include <vector>
 
+//3 farklı türden donksiyon içeren (public, private, protected) interface (abstract class)
 class IBaseClass {
 public:
     virtual void PublicFunc() {
@@ -23,16 +24,19 @@ protected:
     }
 };
 
+//Interface'i extend'leyen class 1
 class Child1 : public IBaseClass {
 public:
     int field1;
 };
 
+//Interface'i extend'leyen class 2
 class Child2 : public IBaseClass {
 public:
     int field2;
 };
 
+//Doubly linked list'in nodları için tanımlanan struct
 struct Node
 {
     IBaseClass* data;
@@ -42,6 +46,7 @@ struct Node
     Node(IBaseClass* d) : data(d), next(nullptr), prev(nullptr) {}
 };
 
+//Doubly linked list sınıfı (her iki sınıftan instance'ların tutulacağı list sınıfı)
 class DoublyLinkedList {
 public:
     Node* head = nullptr;
@@ -60,11 +65,10 @@ public:
         tail = n;
     }
 
+    //field'e göre node aranıp bulunmasını sağlayan metot
     Node* findByField(int value) {
         Node* cur = head;
         while (cur) {
-            // Child1 mi Child2 mi kontrol ederek
-            // unique field'ı karşılaştır
             if (auto c1 = dynamic_cast<Child1*>(cur->data)) {
                 if (c1->field1 == value) return cur;
             }
@@ -82,32 +86,35 @@ std::vector<int> unique_numbers;
 std::list<std::unique_ptr<Child1>> instances_1;
 std::list<std::unique_ptr<Child2>> instances_2;
 
+//Gelen değerin daha önce alınmış olup olmadığını check edip bool değer döndüren sınıf
 bool CheckIfExist(int numToCheck) {
     if (unique_numbers.size() == 0)
         return false;
     return std::find(unique_numbers.begin(), unique_numbers.end(), numToCheck) != unique_numbers.end();
 }
 
+//CheckIfExist fonksiyonundan da faydalanarak gereken unique random integer'ın return edilmesini sağlayan fonksiyon
 int GetUniqueRandom() {
     int randomNum;
     do
     {
         randomNum = rand() % 200 + 1;
-        //std::cout << CheckIfExist(randomNum) << std::endl;
     } while (CheckIfExist(randomNum));
     return randomNum;
 }
 
 int main()
 {
-    srand(time(0));
+    srand(time(0)); //random değerlerin program her çalıştığında farklı random üretmesini sağlayan kod
     DoublyLinkedList dll;
 
+    //10 kere dönecek, bir saniyelik periyodu da içinde kapsamış bulunan for döngüsü
     for (int i = 1; i <= 10; i++) {
         int numA = rand() % 10 + 1;
         int numB = rand() % 10 + 1;
+
+        //Child1 Instance'larının ve bunlara istenen şekilde değer atanmasını sağlayan for döngüsü
         for (int j = 1; j <= numA; j++) {
-            std::cout << "loop_a =" << j << std::endl;
             auto instance = std::make_unique<Child1>();
             instance->field1 = GetUniqueRandom();
             unique_numbers.push_back(instance->field1);
@@ -115,8 +122,8 @@ int main()
             instances_1.push_back(std::move(instance));
         }
         
+        //Child2 Instance'larının ve bunlara istenen şekilde değer atanmasını sağlayan for döngüsü
         for (int j = 1; j <= numB; j++) {
-            std::cout << "loop_b =" << j << std::endl;
             auto instance = std::make_unique<Child2>();
             instance->field2 = GetUniqueRandom();
             unique_numbers.push_back(instance->field2);
@@ -124,18 +131,19 @@ int main()
             instances_2.push_back(std::move(instance));
         }
 
+        std::this_thread::sleep_for(std::chrono::seconds(1));   //Döngünün birer saniyelik preiyotlarla çalışmasını sağlayan kod satırı
+        std::cout << "Instance'lar olusturuldu ve ilk dongu tamamlandı" << std::endl << "Kalan dongu: " << 10 - i << std::endl;
+    }
+        //Aranacak değeri random olarak belirleme ve konsola bastırma
         int randomValue = unique_numbers[rand() % unique_numbers.size()];
-        std::cout << "Searching for: " << randomValue << std::endl;
+        std::cout << randomValue << " araniyor." << std::endl;
 
-        // DLL içinde ara
+        //Node'un doubly linked list içerisinde aranması
         Node* found = dll.findByField(randomValue);
         if (found) {
-            std::cout << "Found instance!" << std::endl;
+            std::cout << "Instance bulundu!" << std::endl;
         }
         else {
-            std::cout << "Not found!" << std::endl;
+            std::cout << "Instance bulunamadı! :(" << std::endl;
         }
-
-        //std::this_thread::sleep_for(std::chrono::seconds(1));
-    }
 }
